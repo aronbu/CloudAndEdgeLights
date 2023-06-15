664d514e61a8a670da5642497126eb677d4d0c50
aronb <aron.buffel@student.howest.be>
2023-06-15 13:28:38 +0200

    fix font

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,41 +1,44 @@
 import './navbar.css'
-import {Link, NavLink} from "react-router-dom";
-import Logo from '../../Assets/Logo/LogoBuffaloLights.svg'
-import {FaUserShield} from "react-icons/fa";
-import {MdLightbulbOutline} from "react-icons/md";
+import { Link, NavLink } from "react-router-dom";
+import { FaUserShield } from "react-icons/fa";
+import { MdLightbulbOutline } from "react-icons/md";
 import { ReactSVG } from 'react-svg';
+import Logo from '../../Assets/Logo/LogoBuffaloLights.svg';
 
 const Navbar = (props) => {
-
-    return(
-
+    return (
         <div className="navbar1">
             <nav>
-                <div className={props.lightDarkMode === 'light' ? 'container nav_container light' : 'container nav_container dark'}>
-                    <Link to="/" className='logo'>
-                        <ReactSVG className="color-logo" src={Logo}  alt="Nav Logo"/>
+                <div className={`container nav_container ${props.lightDarkMode === 'light' ? 'light' : 'dark'}`}>
+                    <Link to="/" className="logo">
+                        <ReactSVG className="color-logo" src={Logo} alt="Nav Logo" />
                     </Link>
                     <ul className="nav_links">
                         <li>
-                            <NavLink to="/" className="menuItem"><span className="text">Lights</span><MdLightbulbOutline/></NavLink>
+                            <NavLink to="/" className="menuItem">
+                                <span className="text">Lights</span>
+                                <MdLightbulbOutline />
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to="/" className="menuItem"><span className="text">Security</span><FaUserShield/></NavLink>
+                            <NavLink to="/" className="menuItem">
+                                <span className="text">Security</span>
+                                <FaUserShield />
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
             </nav>
-<style>{`
-    .color-logo svg{
-        fill: ${props.color};
-}
-.navbar1 .nav_container .nav_links li a{
-    color: ${props.color};
-    }
-`}
-</style>
+            <style>{`
+        .color-logo svg {
+          fill: ${props.color};
+        }
+        .navbar1 .nav_container .nav_links li a {
+          color: ${props.color};
+        }
+      `}</style>
         </div>
+    );
+};
 
-    )
-}
-export default Navbar
+export default Navbar;
